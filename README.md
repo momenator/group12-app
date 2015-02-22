0b3571abc1e4c8a95e736f9c885d5cafb649f752
mombi93 <m.d.a.rafdi@gmail.com>
2015-02-22 21:18:30 +0000

    added 404 and 500 error handler

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # COMP 2013 Systems Engineering Group 12
-## Group members: Muhammad Rafdi, Ali Sarraf and James Durrant. 
+
+Group members: Muhammad Rafdi, Ali Sarraf and James Durrant. 
--- server.js
+++ server.js
@@ -3,6 +3,19 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+ 
+app.use(function(req,res){
+	res.type('text/plain');
+	res.status(404);
+	res.send('Error 404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('Error 505 - Server Error');
+});
 
 app.listen(app.get('port'), function(){
 	console.log('App running on port ' + app.get('port') );
