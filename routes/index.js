f126b639576832713fc058125cd46e9f5dbd39b4
mombi93 <m.d.a.rafdi@gmail.com>
2015-03-02 11:51:17 +0000

    added several UI elements

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var AlchemyAPI = require('../lib/alchemy/alchemyapi');
 var alchemyapi = new AlchemyAPI();
+var unirest = require('unirest');
 var request = require('request');
 var express = require('express');
 var router = express.Router();
@@ -25,8 +26,6 @@ router.get('/alchemy', function(req, res, next) {
 router.get('/camfind',function(req, res, next) {
 	request('http://bldata.herokuapp.com/images/random', function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
-	    console.log("original jpeg > " + JSON.parse(response.body).flickr_original_jpeg); 
-	    console.log(JSON.parse(response.body).flickr_thumb_jpeg);// Show the HTML for the Google homepage. 
 		var url = JSON.parse(response.body).flickr_thumb_jpeg;
 		request.post({
 			url : 'https://camfind.p.mashape.com/image_requests',
@@ -49,19 +48,33 @@ router.get('/camfind',function(req, res, next) {
 			console.log(body);
 			console.log(JSON.parse(body));
 			console.log(JSON.parse(body).token);
-			var uri = JSON.parse(body).token;
-			request.get({
-				url: 'https://camfind.p.mashape.com/image_responses/' + uri,
+			var uri = 'https://camfind.p.mashape.com/image_responses/';
+			console.log('uri >>>' + uri.concat(JSON.parse(body).token));
+			var uriFinal = uri.concat(JSON.parse(body).token) ;
+			/*
+			var options = {
+				url: uriFinal ,
 				headers :{
 					'X-Mashape-Key':'0PHOUEsrVsmsh4kV3i9X5FFqfof4p1maU5Vjsn62WwTuW8AIxn',
 					'Accept' : 'application/json'
 				}
-			}, function (error, httpResponse, body){
+			}
+			request(options, function (error, httpResponse, body){
 				console.log(body);
-				res.render('image' , { title : 'Camfind API', url: url} );
 			});
-			
+			*/
+			console.log("var :: " + uriFinal);
+			console.log("string :: " + "https://camfind.p.mashape.com/image_responses/" + "x06pZGUbLV-85wAdqhxEDw");
+
+			unirest.get("https://camfind.p.mashape.com/image_responses/" + "x06pZGUbLV-85wAdqhxEDw")
+			.header("X-Mashape-Key", "0PHOUEsrVsmsh4kV3i9X5FFqfof4p1maU5Vjsn62WwTuW8AIxn")
+			.header("Accept", "application/json")
+			.end(function (result) {
+				console.log(result.status, result.headers, result.body);
+			});			
 		});
+		res.render('image' , { title : 'Camfind API', url: 'http://d1spq65clhrg1f.cloudfront.net/uploads/image_request/image/22/22700/22700121/11307056475_a8bca6fbd9_q.jpg'} );
+
 	  }
 	});
 
