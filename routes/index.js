9e15065e8fe6d0721ea519c7a758225001939039
mombi93 <m.d.a.rafdi@gmail.com>
2015-03-03 11:21:06 +0000

    removed camfind

--- routes/index.js
+++ routes/index.js
@@ -23,64 +23,6 @@ router.get('/alchemy', function(req, res, next) {
 	});
 });
 
-router.get('/camfind',function(req, res, next) {
-	request('http://bldata.herokuapp.com/images/random', function (error, response, body) {
-	  if (!error && response.statusCode == 200) {
-		var url = JSON.parse(response.body).flickr_thumb_jpeg;
-		request.post({
-			url : 'https://camfind.p.mashape.com/image_requests',
-			headers: {
-				'X-Mashape-Key':'0PHOUEsrVsmsh4kV3i9X5FFqfof4p1maU5Vjsn62WwTuW8AIxn',
-				'Content-Type' :'application/x-www-form-urlencoded',
-				'Accept' : 'application/json'
-			},
-			form:{
-				focus : {
-					x: 480,
-					y: 640
-				},
-				image_request : {
-					locale: 'en_US',
-					remote_image_url : url
-				}
-			}
-		}, function (error, httpResponse, body){
-			console.log(body);
-			console.log(JSON.parse(body));
-			console.log(JSON.parse(body).token);
-			var uri = 'https://camfind.p.mashape.com/image_responses/';
-			console.log('uri >>>' + uri.concat(JSON.parse(body).token));
-			var uriFinal = uri.concat(JSON.parse(body).token) ;
-			/*
-			var options = {
-				url: uriFinal ,
-				headers :{
-					'X-Mashape-Key':'0PHOUEsrVsmsh4kV3i9X5FFqfof4p1maU5Vjsn62WwTuW8AIxn',
-					'Accept' : 'application/json'
-				}
-			}
-			request(options, function (error, httpResponse, body){
-				console.log(body);
-			});
-			*/
-			console.log("var :: " + uriFinal);
-			console.log("string :: " + "https://camfind.p.mashape.com/image_responses/" + "x06pZGUbLV-85wAdqhxEDw");
-
-			unirest.get("https://camfind.p.mashape.com/image_responses/" + "x06pZGUbLV-85wAdqhxEDw")
-			.header("X-Mashape-Key", "0PHOUEsrVsmsh4kV3i9X5FFqfof4p1maU5Vjsn62WwTuW8AIxn")
-			.header("Accept", "application/json")
-			.end(function (result) {
-				console.log(result.status, result.headers, result.body);
-			});			
-		});
-		res.render('image' , { title : 'Camfind API', url: 'http://d1spq65clhrg1f.cloudfront.net/uploads/image_request/image/22/22700/22700121/11307056475_a8bca6fbd9_q.jpg'} );
-
-	  }
-	});
-
-});
-
-
 router.get('/imagga',function(req, res, next) {
 	request('http://bldata.herokuapp.com/images/random', function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
