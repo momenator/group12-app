f195a0044bdaf9edc5b97a8b4bfd70b7ddb7be86
mombi93 <m.d.a.rafdi@gmail.com>
2015-03-16 01:26:31 +0000

    css and js cleanup

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,19 @@ var MongoClient = mongo.MongoClient;
 var client = new MongoClient();
 var db;
 
+var shortenStringHelper = function (string) {
+	return String(string).substring(0, 35) + "..."
+}; 
+
+var shortenDecimalHelper = function (num) { 
+	return Number(num).toFixed(2); 
+}
+
+var convertToPercentageHelper = function (num) { 
+	return Number(num*100).toFixed(2);
+}
+
+
 client.connect("mongodb://***:***@ds049171.mongolab.com:49171/bl-dataset",function(err, db) {
     if(err) {
     	console.log(err);
@@ -36,7 +49,7 @@ router.post('/search', function(req, res, next){
 	}).limit(250).toArray(function (err, docs){
 		if (err) {
 			console.log (err);
-			console.log(req.body.query);
+			console.log("searched " + req.body.query);
 			res.render('error', { message:err });
 		} else {
 			if (docs.length == 0){
@@ -45,17 +58,17 @@ router.post('/search', function(req, res, next){
 					searchType: "title" ,
 					searchQuery: req.body.query,
 					helpers:{
-						shortenString : function (string) {return String(string).substring(0, 35) + "..."} 
+						shortenString : shortenStringHelper 
 					} 
 				});
 			} else {
-				console.log(req.body.query);
+				console.log("searched " + req.body.query);
 				res.render('search',{
 					imageResults : docs,
 					searchType: "title" ,
 					searchQuery: req.body.query,
 					helpers:{
-						shortenString : function (string) {return String(string).substring(0, 35) + "..."} 
+						shortenString : shortenStringHelper
 					} 
 				});
 			}
@@ -73,7 +86,7 @@ router.post('/searchAlchemyTags', function(req, res, next){
 	}).limit(250).toArray(function (err, docs){
 		if (err) {
 			console.log (err);
-			console.log(req.body.query);
+			console.log("searched " + req.body.query);
 			res.render('error', { message:err });
 		} else {
 			if (docs.length == 0){
@@ -82,17 +95,17 @@ router.post('/searchAlchemyTags', function(req, res, next){
 					searchType: "Alchemy API", 
 					searchQuery: req.body.query,
 					helpers:{
-						shortenString : function (string) {return String(string).substring(0, 35) + "..."} 
+						shortenString : shortenStringHelper 
 					}  
 				});
 			} else {
-				console.log(req.body.query);
+				console.log("searched " + req.body.query);
 				res.render('search',{
 					imageResults : docs, 
 					searchType: "Alchemy API", 
 					searchQuery: req.body.query,
 					helpers:{
-						shortenString : function (string) {return String(string).substring(0, 35) + "..."} 
+						shortenString : shortenStringHelper
 					}  
 				});
 			}
@@ -119,7 +132,7 @@ router.post('/searchImaggaTags', function(req, res, next){
 					searchType: "Imagga API", 
 					searchQuery: req.body.query,
 					helpers:{
-						shortenString : function (string) {return String(string).substring(0, 35) + "..."} 
+						shortenString : shortenStringHelper
 					}  
 				});
 			} else {
@@ -129,7 +142,7 @@ router.post('/searchImaggaTags', function(req, res, next){
 					searchType: "Imagga API",
 					searchQuery: req.body.query,
 					helpers:{
-						shortenString : function (string) {return String(string).substring(0, 35) + "..."} 
+						shortenString : shortenStringHelper 
 					} 
 				});
 			}
@@ -137,30 +150,10 @@ router.post('/searchImaggaTags', function(req, res, next){
 	});
 });
 
-var getTagsAlchemyAPI = function (url){
-	alchemyapi.image_keywords('url', url, {}, function (response) {	
-		return response.imageKeywords;
-	}); 
-}
-
-
-var getTagsImaggaAPI = function (url){
-	request.get({
-	    url:'https://api.imagga.com/v1/tagging?url='+ url,
-	    auth: {
-	    	username:"acc_3a8a3280ff382f5",
-	    	password: "a5c945ee52846e612ff5705d6ce2e1a8"
-	    }
-	}, function (err, httpResponse, body) {
-		var response = JSON.parse(body);
-	    return response.results[0].tags;
-	});
-}
-
 router.get('/search/:imageid',function(req, res, next){
 	collection.findOne({'_id':new objectId(req.params.imageid)}, function (err, doc){
-		console.log(doc);
 		var url = doc.flickr_small_source;
+		console.log ("Searched " + req.params.imageid);
 		if (err) {
 			console.log (err);
 			res.render('error', { message:err } );
@@ -178,14 +171,13 @@ router.get('/search/:imageid',function(req, res, next){
 				alchemyTags : doc.alchemyTags,
 				imaggaTags : doc.imaggaTags,
 				helpers: {
-		            shortenDecimal: function (num) { return Number(num).toFixed(2); },
-		            convertToPercentage : function (num) { return Number(num*100).toFixed(2); }
+		            shortenDecimal: shortenDecimalHelper,
+		            convertToPercentage : convertToPercentageHelper
 		        }
 			});
 		} else {
 			alchemyapi.image_keywords('url', url, {}, function (response) {	
 				var alchemyTags = response.imageKeywords;
-				console.log(alchemyTags);
 				request.get({
 				    url:'https://api.imagga.com/v1/tagging?url='+ url,
 				    auth: {
@@ -194,7 +186,6 @@ router.get('/search/:imageid',function(req, res, next){
 				    }
 				}, function (err, httpResponse, body) {
 					var imaggaTags = JSON.parse(body).results[0].tags;
-					console.log(imaggaTags);
 					collection.update({'_id':new objectId(req.params.imageid)}, {$set : {alchemyTags: alchemyTags, imaggaTags: imaggaTags}},function(){console.log('inserted tags')});
 					res.render('image', { 
 						url: doc.flickr_original_source, 
@@ -209,8 +200,8 @@ router.get('/search/:imageid',function(req, res, next){
 						alchemyTags : alchemyTags,
 						imaggaTags : imaggaTags,
 						helpers: {
-		            		shortenDecimal: function (num) { return Number(num).toFixed(2); },
-		            		convertToPercentage : function (num) { return Number(num).toFixed(2)*100.00; }
+		            		shortenDecimal: shortenDecimalHelper,
+		            		convertToPercentage : convertToPercentageHelper
 				        }
 					});
 				});
@@ -220,59 +211,4 @@ router.get('/search/:imageid',function(req, res, next){
 	});
 });
 
-/* 
-
-// This is the script to automate the tagging process.. currently under maintenance
-
-router.get('/analyse/:keyword', function(req, res, next){
-	collection.find({
-		"$text" : {
-			"$search" : req.params.keyword
-		}
-	}).toArray(function (err, docs){
-		if (err) {
-			console.log (err);
-			console.log(req.body.query);
-			res.render('error', { message:err });
-		} else {
-			if (docs.length == 0){
-				res.send('no results found');
-			} else {
-				console.log(req.body.keyword);
-				res.send("analysis in process");
-				//for (var i = 0; i < (docs.length - 1); i++){
-					analyseDocsFromQuery(docs);
-				//}
-			}
-		}
-	});
-});
-
-analyseDocsFromQuery = function(docs){
-	for (var i = 0; i < docs.length; ){
-		var url = docs[i].flickr_small_source;
-		var currId = docs[i]._id;
-		console.log("current url " + url);
-		console.log("current ID: " + currId );
-		alchemyapi.image_keywords('url', url, {}, function (response) {	
-			var alchemyTags = response.imageKeywords;
-			//console.log(alchemyTags);
-			request.get({
-			    url:'https://api.imagga.com/v1/tagging?url='+ url,
-			    auth: {
-			    	username:"acc_3a8a3280ff382f5",
-			    	password: "a5c945ee52846e612ff5705d6ce2e1a8"
-			    }
-			}, function (err, httpResponse, body) {
-				if (JSON.parse(body).results[0].tags != undefined){
-					var imaggaTags = JSON.parse(body).results[0].tags;
-					collection.update({'_id':new objectId(currId) }, {$set : {alchemyTags: alchemyTags, imaggaTags: imaggaTags}}, function(){ i++; console.log("tags inserted. Analysis progress: " + i + "of " + docs.length + " done." )});
-				}
-				//console.log(imaggaTags);
-			});
-
-		});
-	}
-}
-*/
 module.exports = router;
