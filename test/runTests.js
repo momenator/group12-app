099f94f1c3a41d6aaf0156e85c635346b6f2e664
AliSarraf <alisarraf@outlook.com>
2015-03-23 17:33:23 +0000

    added test for all the http endpoints that use GET method

--- test/runTests.js
+++ test/runTests.js
@@ -4,11 +4,42 @@ var should = require('should'),
 	supertest = require('supertest');
 
 describe('group12-app', function () {
+	/* test all the end points */
 	it('should successfully calls the home page', 
 	function (done) {
 		supertest(app)
 		.get('/')
 		.expect(200)
+		.end(function (err, res) {
+			done();
+		});
+	});
+
+	it('should successfully calls the search page', 
+	function (done) {
+		supertest(app)
+		.get('/search')
+		.expect(200)
+		.end(function (err, res) {
+			done();
+		});
+	});
+
+	it('should successfully calls the image with id 54f615fb4bdf80530b020176 page', 
+	function (done) {
+		supertest(app)
+		.get('/search/54f615fb4bdf80530b020176')
+		.expect(200)
+		.end(function (err, res) {
+			done();
+		});
+	});
+
+	it('should redirect to error 404 page', 
+	function (done) {
+		supertest(app)
+		.get('/randompagethatdoesntexist')
+		.expect(404)
 		.end(function (err, res) {
 			done();
 		});
