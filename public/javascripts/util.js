23d66039eeac81ce4f970736c66a11542add2828
mombi93 <m.d.a.rafdi@gmail.com>
2015-04-05 20:56:31 +0100

    more statistics stuff and modal

--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -3,7 +3,7 @@ $(window).load(function(){
 });
 
 $(document).ready(function(){
-	 $('input[type="radio"]').click(function(){
+	$('input[type="radio"]').click(function(){
         if($(this).attr("value")=="title"){
         	console.log("title");
             $('#query').attr("action", "/search");
@@ -13,5 +13,14 @@ $(document).ready(function(){
 
             $('#query').attr("action", "/searchTags");
         }
+    });
+    $('.tag').click(function(){
+        var keywords = String($(this).html()).split(" ");
+        var tagName = keywords[0];
+        if (keywords[1] != ':' && keywords[1] != ' '){
+            tagName = keywords[0] + ' ' + keywords[1];
+        }
+        $('.tag-name').html('"' + tagName + '"');
+        $('.search-tag-btn').attr("href", "/searchTags/" + tagName);
     });
 });
--- routes/index.js
+++ routes/index.js
@@ -287,6 +287,6 @@ module.exports = function (collection){
 			}
 		});
 	};
-
+	
 	return functions;
 }
